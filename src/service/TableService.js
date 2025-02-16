d022e42c652545ba2759694276e3978756d615f0
Hamza Şahin <hamza.sahin@hakanbt.com.tr>
2025-02-16 20:54:05 +0300

    refactor TableSerVice remove unnecessary try catch statements

--- src/service/TableService.js
+++ src/service/TableService.js
@@ -1,72 +1,50 @@
 import { axiosInstance } from '@/api/axiosInstance';
 
-const  path = 'desk;';
-var url = "";
+const path = 'desk;';
 
 const TableService = {
     async createTable(postData) {
-        try {
-            const formData = new FormData();
-            formData.append('name', postData.name);
-            formData.append('capacity', postData.capacity);
-            const response = await axiosInstance.post(path, formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                },
-            });
+        const formData = new FormData();
+        formData.append('name', postData.name);
+        formData.append('capacity', postData.capacity);
+        const response = await axiosInstance.post(path, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        });
 
-            return response.data;
-        } catch (error) {
-            throw error;
-        }
+        return response.data;
     },
 
-
-
     async getTable(tableId) {
-        try {
-           let url = path + "/" + tableId;
-            const response = await axiosInstance.get(url);
-            return response.data;
-        } catch (error) {
-            return error;
-        }
+        let url = path + '/' + tableId;
+        const response = await axiosInstance.get(url);
+        return response.data;
     },
 
     async getTableList() {
-        try {
-            const  response =  await  axiosInstance.get(path);
-            return response.data;
-        } catch (error) {
-            return error;
-        }
+        const response = await axiosInstance.get(path);
+        return response.data;
     },
 
     async updateTable(tableId, postData) {
-        try {
-            const formData = new FormData();
-            formData.append('name', postData.name);
-            formData.append('capacity', postData.capacity);
-            url = path + "/" + tableId;
-            const response = await axiosInstance.put(url, formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                },
-            });
-            return response.data;
-        } catch (error) {
-            return error;
-        }
+        const formData = new FormData();
+        formData.append('name', postData.name);
+        formData.append('capacity', postData.capacity);
+        url = path + '/' + tableId;
+        const response = await axiosInstance.put(url, formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        });
+        return response.data;
     },
 
     async deleteTable(tableId) {
-        try {
-            let url = path + "/" + tableId;
-            const response = await axiosInstance.delete(url);
-            return response.data;
-        } catch (error) {
-            return error;
-        }
+        let url = path + '/' + tableId;
+        const response = await axiosInstance.delete(url);
+        return response.data;
     }
 };
+
 export default TableService;
